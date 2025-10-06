06ea3b17d5edef1125197212c3abc5f5f141b190
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-06 08:28:52 -0300

    first commit

---
+++ src/main/java/com/example/empregados_tdd_exercicio/EmpregadosTddExercicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.empregados_tdd_exercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpregadosTddExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpregadosTddExercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/empregados_tdd_exercicio/EmpregadosTddExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empregados_tdd_exercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpregadosTddExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

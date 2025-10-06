9dc171d76756086a10edb2d967d842fe6a58d44a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-06 11:39:12 -0300

    Employee search by id

--- src/main/java/com/example/empregados_tdd_exercicio/controllers/EmployeeController.java
+++ src/main/java/com/example/empregados_tdd_exercicio/controllers/EmployeeController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/employees")
@@ -21,6 +18,12 @@ public class EmployeeController {
     @GetMapping
     public ResponseEntity<Page<EmployeeDTO>> findAll(@RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
         Page<EmployeeDTO> dto = employeeService.findAllByPage(name, pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<EmployeeDTO> findById(@PathVariable Long id) {
+        EmployeeDTO dto = employeeService.findById(id);
         return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/example/empregados_tdd_exercicio/services/EmployeeService.java
+++ src/main/java/com/example/empregados_tdd_exercicio/services/EmployeeService.java
@@ -4,11 +4,14 @@ import com.example.empregados_tdd_exercicio.dto.EmployeeDTO;
 import com.example.empregados_tdd_exercicio.entities.Employee;
 import com.example.empregados_tdd_exercicio.repositories.DepartmentRepository;
 import com.example.empregados_tdd_exercicio.repositories.EmployeeRepository;
+import com.example.empregados_tdd_exercicio.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -22,5 +25,13 @@ public class EmployeeService {
     public Page<EmployeeDTO> findAllByPage(String name, Pageable pageable) {
         Page<Employee> employees = employeeRepository.searchByName(name, pageable);
         return employees.map(EmployeeDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public EmployeeDTO findById(Long id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+        Employee employee = result.orElseThrow(() -> new ResourceNotFoundException("Resource not found!"));
+        EmployeeDTO dto = new EmployeeDTO(employee);
+        return dto;
     }
 }

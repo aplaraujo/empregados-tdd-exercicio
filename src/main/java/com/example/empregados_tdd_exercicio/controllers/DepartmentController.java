d4df0c532408aa6fe95d4536da16ce32bd856944
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-06 09:52:23 -0300

    Department search by id

--- src/main/java/com/example/empregados_tdd_exercicio/controllers/DepartmentController.java
+++ src/main/java/com/example/empregados_tdd_exercicio/controllers/DepartmentController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,12 @@ public class DepartmentController {
     @GetMapping
     public ResponseEntity<Page<DepartmentDTO>> findAll(Pageable pageable) {
         Page<DepartmentDTO> dto = departmentService.findAllByPage(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DepartmentDTO> findById(@PathVariable Long id) {
+        DepartmentDTO dto = departmentService.findById(id);
         return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/example/empregados_tdd_exercicio/services/DepartmentService.java
+++ src/main/java/com/example/empregados_tdd_exercicio/services/DepartmentService.java
@@ -3,6 +3,7 @@ package com.example.empregados_tdd_exercicio.services;
 import com.example.empregados_tdd_exercicio.dto.DepartmentDTO;
 import com.example.empregados_tdd_exercicio.entities.Department;
 import com.example.empregados_tdd_exercicio.repositories.DepartmentRepository;
+import com.example.empregados_tdd_exercicio.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,4 +24,11 @@ public class DepartmentService {
         return departments.map(dep -> new DepartmentDTO(dep));
     }
 
+    @Transactional(readOnly = true)
+    public DepartmentDTO findById(Long id) {
+        Optional<Department> result = departmentRepository.findById(id);
+        Department department = result.orElseThrow(() -> new ResourceNotFoundException("Resource not found!"));
+        DepartmentDTO dto = new DepartmentDTO(department);
+        return dto;
+    }
 }

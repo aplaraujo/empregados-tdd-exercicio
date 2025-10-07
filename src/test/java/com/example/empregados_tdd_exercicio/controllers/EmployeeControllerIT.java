30689e7987a948e74e8cdd397b60e54913f15b54
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-07 07:57:25 -0300

    Tests passing

--- src/test/java/com/example/empregados_tdd_exercicio/controllers/EmployeeControllerIT.java
+++ src/test/java/com/example/empregados_tdd_exercicio/controllers/EmployeeControllerIT.java
@@ -1,5 +1,8 @@
 package com.example.empregados_tdd_exercicio.controllers;
 
+import com.example.empregados_tdd_exercicio.dto.EmployeeDTO;
+import com.example.empregados_tdd_exercicio.tests.factory.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,10 @@ public class EmployeeControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private long existingId;
     private long notExistingId;
     private long countTotalEmployees;
@@ -43,5 +50,21 @@ public class EmployeeControllerIT {
                 .andExpect(jsonPath("$.content[0].name").value("Alex"))
                 .andExpect(jsonPath("$.content[1].name").value("Ana"))
                 .andExpect(jsonPath("$.content[2].name").value("Andressa"));
+    }
+
+    @Test
+    public void insertShouldCreateResource() throws Exception {
+        EmployeeDTO dto = Factory.createEmployeeDTO();
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/employees")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.email").exists());
     }
 }
